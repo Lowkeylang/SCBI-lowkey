754f40c66f88dd69e911c298b6561e655426577e
bossransuper2024 <bossransuper2024@gmail.com>
2025-02-18 12:31:31 +0800

    Update SCBI_lowkey_000.lua

--- SCBI_lowkey_000.lua
+++ SCBI_lowkey_000.lua
@@ -1,1 +1,21 @@
+UpdatesLog = {
+    "\n\nVersion : 1\n- Default version",
+}
 
+if ScriptVersion == #UpdatesLog then
+else
+    ChangeLog = "✅ New version Avilable ✅\n"
+    ChangeLog = ChangeLog.."Current Version : "..ScriptVersion.."\n"
+    ChangeLog = ChangeLog.."Latest Version : "..#UpdatesLog
+
+    LoopChangeLog = #UpdatesLog
+
+    while ScriptVersion  <  LoopChangeLog do
+        ChangeLog = ChangeLog..UpdatesLog[LoopChangeLog]
+        LoopChangeLog = LoopChangeLog - 1
+    end
+	gg.alert(ChangeLog, "Get Link")
+    gg.setVisible(false)
+  --  gg.copyText("https://t.me/Hackers_House_YT")
+
+end
